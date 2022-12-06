d57fc076f3a5d0a35a4dd439b5b3b04d2f4ea434
Gungwahada1 <gungwahada1@gmail.com>
2022-12-06 21:48:21 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      "Bebas+Neue": "Bebas Neue",
+      Nunito: "Nunito",
+      Poppins: "Poppins",
+    },
+    colors: {
+      "60%": "#06283D",
+      "30%": "#256D85",
+      "10%": "#F3F3F3",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
